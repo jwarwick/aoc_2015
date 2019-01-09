2006a12f698c65ead9112bd22b8365572c660fcc
John Warwick <jwarwick@gmail.com>
2019-01-09 17:32:36 -0500

    Day 3, part 2

--- cmd/day03/day03.go
+++ cmd/day03/day03.go
@@ -10,18 +10,25 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	part1 := housesDeliveredTo(string(input))
+	part1 := housesDeliveredTo(string(input), 1)
 	fmt.Println("Part 1: ", part1)
 
-	// part2 := day02.RibbonSum(string(input))
-	// fmt.Println("Part 2: ", part2)
+	part2 := housesDeliveredTo(string(input), 2)
+	fmt.Println("Part 2: ", part2)
 }
 
-func housesDeliveredTo(input string) int {
-	houses := make(map[complex128]int)
-	curr := 0 + 0i
-	houses[curr] = 1
+func housesDeliveredTo(input string, num int) int {
+	houses := make(map[complex128]int, len(input))
+	locs := make([]complex128, num)
+	for i := 0; i < num; i++ {
+		locs[i] = 0 + 0i
+		houses[locs[i]] += 1
+	}
+	curr_loc := 0
+
 	for _, c := range input {
+		idx := curr_loc % len(locs)
+		curr := locs[idx]
 		switch c {
 		case '^':
 			curr += 0 + 1i
@@ -33,6 +40,8 @@ func housesDeliveredTo(input string) int {
 			curr += 1 + 0i
 		}
 		houses[curr] += 1
+		locs[idx] = curr
+		curr_loc += 1
 	}
 	return len(houses)
 }
--- cmd/day03/day03_test.go
+++ cmd/day03/day03_test.go
@@ -15,7 +15,25 @@ func TestHousesDeliveredTo(t *testing.T) {
 	}
 
 	for _, table := range tables {
-		result := housesDeliveredTo(table.input)
+		result := housesDeliveredTo(table.input, 1)
+		if result != table.result {
+			t.Errorf("Input: %s, expected: %d, got: %d", table.input, table.result, result)
+		}
+	}
+}
+
+func TestRoboHousesDeliveredTo(t *testing.T) {
+	tables := []struct {
+		input  string
+		result int
+	}{
+		{"^v", 3},
+		{"^>v<", 3},
+		{"^v^v^v^v^v", 11},
+	}
+
+	for _, table := range tables {
+		result := housesDeliveredTo(table.input, 2)
 		if result != table.result {
 			t.Errorf("Input: %s, expected: %d, got: %d", table.input, table.result, result)
 		}
