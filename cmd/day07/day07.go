bf1046ebb748a02aafbda22bd3e0cde78e3bfdca
John Warwick <jwarwick@gmail.com>
2019-01-14 17:28:14 -0500

    Day 7, part 1

---
+++ cmd/day07/day07.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	part1 := simulate(string(input), "a")
+	fmt.Println("Part 1: ", part1["a"])
+}
+
+var gates map[string]gate
+var wires map[string]uint16
+
+func simulate(input string, target string) map[string]uint16 {
+	gates = make(map[string]gate)
+	wires = make(map[string]uint16)
+
+	lines := splitInput(input)
+
+	for _, l := range lines {
+		g := parseGate(l)
+		gates[g.output] = g
+	}
+
+	g := gates[target]
+	g.evaluate()
+
+	return wires
+}
+
+func splitInput(input string) []string {
+	trimmed := strings.TrimSpace(input)
+	lines := strings.Split(trimmed, "\n")
+	return lines
+}
+
+type gate struct {
+	input1 string
+	input2 string
+	output string
+	fn     func(string, string) uint16
+}
+
+func parseGate(input string) gate {
+	var g gate
+
+	halves := strings.Split(input, " -> ")
+	g.output = strings.TrimSpace(halves[1])
+	cmd := strings.TrimSpace(halves[0])
+	switch {
+	case strings.Contains(cmd, "AND"):
+		args := strings.Split(cmd, " AND ")
+		g.input1 = args[0]
+		g.input2 = args[1]
+		g.fn = and
+	case strings.Contains(cmd, "OR"):
+		args := strings.Split(cmd, " OR ")
+		g.input1 = args[0]
+		g.input2 = args[1]
+		g.fn = or
+	case strings.Contains(cmd, "NOT"):
+		args := strings.Split(cmd, "NOT ")
+		g.input1 = args[1]
+		g.fn = not
+	case strings.Contains(cmd, "LSHIFT"):
+		args := strings.Split(cmd, " LSHIFT ")
+		g.input1 = args[0]
+		g.input2 = args[1]
+		g.fn = lshift
+	case strings.Contains(cmd, "RSHIFT"):
+		args := strings.Split(cmd, " RSHIFT ")
+		g.input1 = args[0]
+		g.input2 = args[1]
+		g.fn = rshift
+	default:
+		g.input1 = cmd
+		g.fn = constant
+	}
+
+	return g
+}
+
+func (g *gate) evaluate() uint16 {
+	_, present := wires[g.output]
+	if !present {
+		result := g.fn(g.input1, g.input2)
+		wires[g.output] = result
+	}
+	return wires[g.output]
+}
+
+func getValue(input string) uint16 {
+	i, err := strconv.Atoi(input)
+	if nil == err {
+		return uint16(i)
+	}
+
+	_, present := wires[input]
+	if !present {
+		g := gates[input]
+		g.evaluate()
+	}
+	return wires[input]
+}
+
+func parseInt(input string) int {
+	i, err := strconv.Atoi(input)
+	if nil != err {
+		fmt.Println("Not an int: ", input)
+		panic("Failed to parse int")
+	}
+	return i
+}
+
+func constant(input string, _ string) uint16 {
+	val := getValue(input)
+	return val
+}
+
+func not(input string, _ string) uint16 {
+	val := getValue(input)
+	return ^val
+}
+
+func and(input1 string, input2 string) uint16 {
+	v1 := getValue(input1)
+	v2 := getValue(input2)
+	return v1 & v2
+}
+
+func or(input1 string, input2 string) uint16 {
+	v1 := getValue(input1)
+	v2 := getValue(input2)
+	return v1 | v2
+}
+
+func lshift(input1 string, input2 string) uint16 {
+	v1 := getValue(input1)
+	v2 := parseInt(input2)
+	return v1 << uint(v2)
+}
+
+func rshift(input1 string, input2 string) uint16 {
+	v1 := getValue(input1)
+	v2 := parseInt(input2)
+	return v1 >> uint(v2)
+}
---
+++ cmd/day07/day07_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCircuit(t *testing.T) {
+	input := `123 -> x
+  456 -> y
+  x AND y -> d
+  x OR y -> e
+  x LSHIFT 2 -> f
+  y RSHIFT 2 -> g
+  y -> z
+  NOT x -> h
+  NOT y -> i`
+
+	vars := []struct {
+		input  string
+		result uint16
+	}{
+		{"d", 72},
+		{"e", 507},
+		{"f", 492},
+		{"g", 114},
+		{"h", 65412},
+		{"i", 65079},
+		{"x", 123},
+		{"y", 456},
+		{"z", 456},
+	}
+
+	for _, v := range vars {
+		result := simulate(input, v.input)
+		if result[v.input] != v.result {
+			t.Errorf("Variable: %s, expected: %d, got: %d", v.input, v.result, result[v.input])
+		}
+	}
+}
