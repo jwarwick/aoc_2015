3103766401fc9172c15af0c7a964ed7a0ca116ea
John Warwick <jwarwick@gmail.com>
2019-01-18 16:07:21 -0500

    Day 12, part 2

--- cmd/day12/day12.go
+++ cmd/day12/day12.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"regexp"
@@ -15,6 +16,9 @@ func main() {
 
 	part1 := stringSum(string(input))
 	fmt.Println("Part 1: ", part1)
+
+	part2 := notRedStringSum(string(input))
+	fmt.Println("Part 2: ", part2)
 }
 
 func stringSum(input string) int {
@@ -28,6 +32,54 @@ func stringSum(input string) int {
 			panic("Not a number")
 		}
 		total += val
+	}
+	return total
+}
+
+func notRedStringSum(input string) int {
+	var i interface{}
+	err := json.Unmarshal([]byte(input), &i)
+	if err != nil {
+		panic("Failed to parse json")
+	}
+	return parseEntry(i)
+}
+
+func parseMap(m map[string]interface{}) int {
+	for _, v := range m {
+		if v == "red" {
+			return 0
+		}
+	}
+
+	total := 0
+	for _, v := range m {
+		total += parseEntry(v)
+	}
+	return total
+}
+
+func parseArray(a []interface{}) int {
+	total := 0
+	for _, v := range a {
+		total += parseEntry(v)
+	}
+	return total
+}
+
+func parseEntry(v interface{}) int {
+	total := 0
+	switch v.(type) {
+	case string:
+	case float64:
+		total += int(v.(float64))
+	case map[string]interface{}:
+		total += parseMap(v.(map[string]interface{}))
+	case []interface{}:
+		total += parseArray(v.([]interface{}))
+	default:
+		fmt.Println("Entry: ", v)
+		panic("Unknown type")
 	}
 	return total
 }
--- cmd/day12/day12_test.go
+++ cmd/day12/day12_test.go
@@ -27,3 +27,30 @@ func TestStringSum(t *testing.T) {
 		}
 	}
 }
+
+func TestNotRed(t *testing.T) {
+	tables := []struct {
+		input  string
+		result int
+	}{
+		{`[1,2,3]`, 6},
+		{`{"a":2,"b":4}`, 6},
+		{`[[[3]]]`, 3},
+		{`{"a":{"b":4},"c":-1}`, 3},
+		{`{"a":[-1,1]}`, 0},
+		{`[-1,{"a":1}]`, 0},
+		{`[-55, {"a":56}]`, 1},
+		{`[]`, 0},
+		{`{}`, 0},
+		{`[1,{"c":"red","b":2},3]`, 4},
+		{`{"d":"red","e":[1,2,3,4],"f":5}`, 0},
+		{`[1,"red",5]`, 6},
+	}
+
+	for _, table := range tables {
+		result := notRedStringSum(table.input)
+		if result != table.result {
+			t.Errorf("Input: %s, expected: %d, got: %d", table.input, table.result, result)
+		}
+	}
+}
