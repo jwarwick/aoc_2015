80f3ab27d7d1e59170ecdac0a3eed1fbb920e8ea
John Warwick <jwarwick@gmail.com>
2019-02-01 14:25:47 -0500

    Day 20, part 2

--- cmd/day20/day20.go
+++ cmd/day20/day20.go
@@ -10,6 +10,39 @@ func main() {
 
 	part1 := findHouse(input)
 	fmt.Println("Part 1: ", part1)
+
+	part2 := part2House(input)
+	fmt.Println("Part 2: ", part2)
+}
+
+func part2House(target int) int {
+	for i := 1; i < 1000000; i++ {
+		sum := divisorSumLimited(i)
+		fmt.Println(i, ":", sum)
+		if sum >= target {
+			return i
+		}
+	}
+	return -1
+}
+
+func divisorSumLimited(value int) int {
+	sum := 0
+	max := int(math.Sqrt(float64(value)))
+	for d := 1; d <= max; d++ {
+		if value%d == 0 {
+			r := int(value / d)
+			if d*50 >= value {
+				sum += (d * 11)
+			}
+			if d != r {
+				if r*50 >= value {
+					sum += (r * 11)
+				}
+			}
+		}
+	}
+	return sum
 }
 
 func findHouse(target int) int {
